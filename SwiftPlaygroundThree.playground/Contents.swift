a9f7b9f592baa8943eba6100ee699154b975beb9
Joseph Payongayong <Jp47@hawaii.edu>
2020-02-18 10:18:56 -1000

    Completed Parts 1-7

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -1,3 +1,25 @@
 import UIKit
-
+//SwiftPlaygroundThree
 var str = "Hello, playground"
+
+//part 2
+//var fruitNames : [String]
+
+//part 3
+var fruitNames = ["Banana" , "Strawberry", "Mango"]
+
+//part 4
+print(fruitNames)
+
+//part 5
+//Desired result to have 3 different lines with the three different fruits
+
+//part 6
+for name in fruitNames
+{
+    print("I like to eat " + name)
+}
+
+//part 7
+print(fruitNames[1])
+print(fruitNames[0])
