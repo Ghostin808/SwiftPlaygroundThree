ee4ed625304ddb52005975db5aadcf56ea3d4603
Joseph Payongayong <spbeachhi@gmail.com>
2020-02-20 10:42:32 -1000

    completed Problem Sets 1-2

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -23,3 +23,19 @@ for name in fruitNames
 //part 7
 print(fruitNames[1])
 print(fruitNames[0])
+
+//Problem Set #1
+var cars = ["Pagani Huayra", "Aston Martin One-77" , "Koenigsegg One:1" , "Ferrari F60 America" , "Mansory Vivere Bugatti Veyron" ," W Motors Lykan Hypersport" ,"Lamborghini Veneno" , "Koenigsegg CCXR Trevita"]
+
+for carNames in cars
+{
+    print(carNames + " is a very expensive car!")
+}
+
+//problem set #2
+var carPrices = ["$1.4M" , "$1.4M" , "$2.0M" ,"$2.5M" , "$3.4M", "$3.4M" , "$4.5M" , "$4.8M"]
+
+for prices in carPrices
+{
+    print(prices + " + 100")
+}
